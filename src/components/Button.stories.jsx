68ccc1cfeede0770c31f74ef54c1ca75152bf112
alpaca1231 <kazuki.amatatsu.fd@gmail.com>
2021-11-23 13:00:26 +0900

    Storybook

---
+++ src/components/Button.stories.jsx
@@ -0,0 +1,10 @@
+import { Button } from './Button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+};
+
+export const ButtonComponent = () => (
+  <Button label="buttonだよ" onClick={() => alert('on click!!')} />
+);
